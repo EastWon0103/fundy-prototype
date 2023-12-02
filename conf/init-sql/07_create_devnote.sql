e90c4dbdaaaed534f3afecfda8e55152181c90c2
EastWon0103 <dongwon000103@gmail.com>
2023-12-02 22:36:27 +0900

    feat: devnote sql init

---
+++ conf/init-sql/07_create_devnote.sql
@@ -0,0 +1,9 @@
+create table devnote
+(
+    id bigint auto_increment primary key,
+    project_id bigint not null,
+    title varchar(255) not null,
+    content varchar(255) not null,
+    created_at datetime not null default current_timestamp,
+    foreign key (project_id) references project(id)
+)
