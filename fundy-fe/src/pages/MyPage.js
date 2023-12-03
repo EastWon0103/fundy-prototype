f1a432597ce28a3b8a323049c628da675b21d7e6
ryanbae94 <ryanbae94@gmail.com>
2023-12-03 18:10:54 +0900

    feat: 후원 기능 추가

--- fundy-fe/src/pages/MyPage.js
+++ fundy-fe/src/pages/MyPage.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Profile from '../components/Profile';
+import useStore from '../store/store';
 
 export default function MyPage() {
+  const getUserInfo = useStore(state => state.getUserInfo);
+
+  useEffect(() => {
+    getUserInfo();
+  }, [getUserInfo])
     
   return (
     <Profile />
