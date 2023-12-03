effe0975da8f1b543f49ed6f9779ffba4a676181
ryanbae94 <ryanbae94@gmail.com>
2023-12-03 18:10:54 +0900

    feat: 후원 기능 추가

--- fundy-fe/src/apis/API.js
+++ fundy-fe/src/apis/API.js
@@ -167,6 +167,12 @@ export const getProjectsById = async (id) => {
 
 }
 
+/**
+ * 리워드 가져오기
+ * @param {int} id 
+ * @returns 
+ */
+
 export const getRewards = async (id) => {
     try {
         const response = await apiClient.get(`/project/${id}/rewards`);
@@ -178,4 +184,30 @@ export const getRewards = async (id) => {
         throw error;
 
     }
+}
+
+/**
+ * 후원하기
+ * @param {int} rewardId 
+ * @param {int} accountId 
+ * @param {int} amount 
+ */
+
+export const funding = async (rewardId, accountId, amount, token) => {
+    const requestBody = {rewardId, accountId, amount}
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }
+    try {
+        const response = await apiClient.post('/fundings', requestBody, config);
+        console.log('펀딩 요청 성공', response.data);
+        
+    } catch (error) {
+        console.log('펀딩요청실패', error)
+        throw error;
+        
+    }
+
 }
--- fundy-fe/src/store/store.js
+++ fundy-fe/src/store/store.js
@@ -1,7 +1,8 @@
 import { create } from 'zustand';
-import { signUp, checkNickname, login, getEmailAuthCode, verifyEmailAuthCode, getUser, getProjectsById } from '../apis/API';
+import { persist } from 'zustand/middleware';
+import { signUp, checkNickname, login, getEmailAuthCode, verifyEmailAuthCode, getUser, funding } from '../apis/API';
 
-const useStore = create(set => ({
+const useStore = create(persist((set, get) => ({
     email: '',
     password: '',
     nickname: '',
@@ -14,17 +15,15 @@ const useStore = create(set => ({
     project: null,
     rewards: null,
 
-    setEmail: email => set(() => ({ email })),
-    setPassword: password => set(() => ({ password })),
-    setNickname: nickname => set(() => ({ nickname })),
-    setCode: code => set(() => ({ code })),
-    setIsValidNickname: isValid => set(() => ({isValidNickname: isValid})),
-    setProject: projectData => set({ project: projectData}),
-    setRewards: rewardsData => set({ rewards: rewardsData}),
-
+    setEmail: (email) => set(() => ({ email })),
+    setPassword: (password) => set(() => ({ password })),
+    setNickname: (nickname) => set(() => ({ nickname })),
+    setCode: (code) => set(() => ({ code })),
+    setIsValidNickname: (isValid) => set(() => ({ isValidNickname: isValid })),
+    setProject: (projectData) => set({ project: projectData }),
+    setRewards: (rewardsData) => set({ rewards: rewardsData }),
 
     checkValidNickname: async () => {
-
         const { nickname } = useStore.getState();
         try {
             const response = await checkNickname(nickname);
@@ -32,7 +31,6 @@ const useStore = create(set => ({
             const isValid = !isDuplicate;
             set({ isValidNickname: isValid });
             return isValid;
-
         } catch (error) {
             console.log('닉네임 중복검사 중 오류 발생', error);
             set({ isValidNickname: false });
@@ -40,90 +38,93 @@ const useStore = create(set => ({
         }
     },
 
-    performEmailAuthCode: async() => {
-        const{ email } = useStore.getState();
+    performEmailAuthCode: async () => {
+        const { email } = useStore.getState();
         try {
             const response = await getEmailAuthCode(email);
             console.log('인증코드 발송 성공', response);
             set({ token: response.result.token })
             return true;
-            
         } catch (error) {
             console.log('인증코드 발송 실패', error.response ? error.response.data : error);
             return false;
         }
     },
 
-    verifyEmailAuthCode: async() => {
+    verifyEmailAuthCode: async () => {
         const { email, token, code } = useStore.getState();
         try {
             const response = await verifyEmailAuthCode(email, token, code);
-            const isSuccess = response.result.verify
+            const isSuccess = response.result.verify;
             set({ isVerifyEmail: isSuccess });
             return isSuccess;
-
         } catch (error) {
             console.log('인증실패', error);
             throw error;
         }
     },
 
-    performSignUp: async() => {
-        const {email, password, nickname, setIsValidNickname} = useStore.getState();
+    performSignUp: async () => {
+        const { email, password, nickname, setIsValidNickname } = useStore.getState();
         try {
             const response = await signUp(email, password, nickname);
-
             console.log('회원가입 성공', response);
             setIsValidNickname(undefined);
-
-            // set({ isLoggedIn: true, user: { nickname }});
-
             return true;
-
-            
         } catch (error) {
             console.log('회원가입 실패', error.response ? error.response.data : error);
             setIsValidNickname(undefined);
-
             return false;
-            
         }
     },
 
-    performLogin: async() => {
-        const {email, password} = useStore.getState();
+    performLogin: async () => {
+        const { email, password } = useStore.getState();
         try {
             const response = await login(email, password);
             console.log('로그인 성공', response);
-
-            set({ isLoggedIn: true });
-            set({ token: response.result.token });
-
+            set({ isLoggedIn: true, token: response.result.token });
             await useStore.getState().getUserInfo();
             return true;
-            
         } catch (error) {
             console.log('로그인 실패', error.response ? error.response.data : error);
-
             return false;
         }
     },
 
-    getUserInfo: async() => {
-        const {token} = useStore.getState();
+    getUserInfo: async () => {
+        const { token } = useStore.getState();
         try {
             const response = await getUser(token);
             console.log('유저정보 불러오기 성공', response);
             set({ user: response.result });
-
         } catch (error) {
-            console.log('유저정보 불러오기 실패', error)
-            
+            console.log('유저정보 불러오기 실패', error);
         }
     },
 
-    
+    performFunding: async (rewardId, amount) => {
+        const { user, token } = useStore.getState();
+        if (user.accounts[0].balance >= amount) {
+            try {
+                await funding(rewardId, user.id, amount, token);
+                console.log('후원 성공')
+                return true;
+                
+            } catch (error) {
+                console.log('후원 실패', error);
+                throw error;
+            } 
+        } else {
+            console.log('잔고 부족');
+            throw new Error('Insufficient balance');
+            
+        }
+    }
+
+}), {
+    name: 'user-store', // 로컬 스토리지에 저장될 키 이름
+    getStorage: () => localStorage, // 사용할 스토리지 엔진
 }));
 
 export default useStore;
-
