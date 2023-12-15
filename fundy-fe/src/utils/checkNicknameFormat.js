ba1b4f59e60f9e71f57cb34415269159c2b5d092
ryanbae94 <ryanbae94@gmail.com>
2023-12-15 19:13:51 +0900

    refactor: auth

--- fundy-fe/src/utils/checkNicknameFormat.js
+++ fundy-fe/src/utils/checkNicknameFormat.js
@@ -1,6 +1,6 @@
-function isValidateNickname(nickname) {
+function checkNicknameFormat(nickname) {
     const nicknameRegex = /^.{2,30}$/;
     return nicknameRegex.test(nickname);
 }
 
-export default isValidateNickname;
+export default checkNicknameFormat;
--- fundy-fe/src/utils/checkPasswordFormat.js
+++ fundy-fe/src/utils/checkPasswordFormat.js
@@ -1,7 +1,7 @@
-function isValidPassword(password) {
+function checkPasswordFormat(password) {
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,30}$/;
     return passwordRegex.test(password);
 }
-export default isValidPassword
+export default checkPasswordFormat;
 
 
